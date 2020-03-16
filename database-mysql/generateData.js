54b4a9ba85ed43c544ad157b966deef489de343a
Ashby Gunter <theshbee@yahoo.com>
2020-03-16 16:05:19 -0700

    Break up data generation functions, refactoring to lessen repeating

--- database-mysql/generateData.js
+++ database-mysql/generateData.js
@@ -1,5 +1,11 @@
 const faker = require('faker');
 
+const generateUSlocation = function () {
+  let state = faker.address.stateAbbr();
+  let city = faker.address.city();
+  let country = 'United States of America';
+  return {country, state, city};
+}
 
 const generateLocations = function () {
   let places = [];
@@ -7,52 +13,55 @@ const generateLocations = function () {
   for (var i = 1; i <= 50; i++) {
     let country = faker.address.country();
     if (country === 'United States of America') {
-      let state = faker.address.stateAbbr();
-      let city = faker.address.city();
-      places.push({country, state, city});
+      places.push(generateUSlocation());
     } else {
       places.push({country});
     }
   }
   for (; i <= 100; i++) {
-    let country = 'United States of America';
-    let state = faker.address.stateAbbr();
-    let city = faker.address.city();
-    places.push({country, state, city});
+    places.push(generateUSlocation());
   }
 
   return places;
 };
 
+const randomNumber = function(max, min = 0) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const generateStartAndEnd = function (daysWeeksMonths) {
+  let start;
+  let end;
+
+  if (daysWeeksMonths === 'days') {
+    start = randomNumber(6, 1);
+    end = randomNumber(start + 1, 13);
+  } else if (daysWeeksMonths === 'weeks') {
+    start = 1;
+    end = randomNumber(3, 2);
+  } else {
+    start = 1;
+    end = 2;
+  }
+  return {start, end};
+}
+
 const generateShipping = function () {
-  // types of delivery - make digital less likely than us or international
   let types = ['US', 'international', 'digital', 'US', 'international'];
   let timeframes = ['days', 'weeks', 'days', 'months', 'days'];
   let shippingOptions = [];
 
-  for (let i = 0; i <= 100; i++) {
-    let type = types[Math.floor(Math.random() * (4 + 1))];
+  for (let i = 0; i < 100; i++) {
+    let type = types[randomNumber(4)];
     let free;
     if (type === 'digital') {
       free = 1; // 'true'
     } else {
-      free = Math.floor(Math.random() * (2));
+      free = randomNumber(1);
     };
-    let timeframe = timeframes[Math.floor(Math.random() * (4 + 1))];
-    let start;
-    let end;
-    if (timeframe === 'days') {
-      start = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-      end = Math.floor(Math.random() * (12 - start + 2) + start + 1);
-    } else if (timeframe === 'weeks') {
-      start = 1;
-      end = Math.floor(Math.random() * (3 - 2 + 1) + 2);
-    } else {
-      start = 1;
-      end = 2;
-    }
-    console.log('start:', start, ', end:', end);
-    timeframe = `${start} - ${end} ${timeframe}`
+    let timeframe = timeframes[randomNumber(4)];
+    let startAndEnd = generateStartAndEnd(timeframe);
+    timeframe = `${startAndEnd.start} - ${startAndEnd.end} ${timeframe}`
     let shipping = {type, free, timeframe};
     shippingOptions.push(shipping);
   }
@@ -60,6 +69,7 @@ const generateShipping = function () {
   return shippingOptions;
 }
 
-console.log(generateShipping());
+// console.log(generateShipping());
+// console.log(generateLocations());
 
 exports.populateDb;
