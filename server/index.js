fc5ec0ef299b52d14a7b93e61df2c500c5270c7d
Ashby Gunter <theshbee@yahoo.com>
2020-03-18 09:49:21 -0700

    Add API for get /items with 200 response
    Added comments for other APIs to put up and goals for them
    Set up script for running nodemon on server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,29 @@ const port = 3004;
 app.use(morgan('dev'));
 
 
+/*
+
+  routes needed:
+GET: /items
+  Returns all items
+GET: /item/:itemId
+  Returns json data with item details
+GET: /shipping/:{itemId, country, code}
+  Returns shipping cost to country and zip/postal code provided
+*/
+
+
+app.get('/items', (req, res) => {
+  res.sendStatus(200);
+});
+
+// app.get('/item/:itemId', (req, res) => {
+//   res.sendStatus(200);
+// });
+
+// app.get('/shipping/:{itemId, country, code}', (req, res) => {
+//   res.sendStatus(200);
+// });
 
 app.listen(port, () => {
   console.log('Express server for item detail now listening on port 3004');
